0c51c9815297cee9127be08769c0e8388114c2a8
shanexuu <Shane.Xu@lincolnuni.ac.nz>
2024-09-01 11:47:46 +1200

    Added transactions

---
+++ savr-app/components/CategoryBtn.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CategoryBtn = () => {
+  return (
+    <View>
+      <Text>CategoryBtn</Text>
+    </View>
+  )
+}
+
+export default CategoryBtn
